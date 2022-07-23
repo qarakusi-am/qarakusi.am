88f25bc04c67fda98f5688fd4419d94dc7732db3
a-arakelian <99004923+a-arakelian@users.noreply.github.com>
2022-07-23 11:44:15 +0400

    Adding simple prob (#22)
    * module with a basic set of objects
    
    * simple problem with parts
    
    * added problems examples
    
    * aramanim - library, with this commit the segment class has been added
    
    * simple woman SVG file
    
    * simple girl SVG file
    
    * added to objects Woman class
    
    * added a problem simple with all attributes for translating text from Armenian to English
    
    * deleted unnecessary files
    
    * Added Scissors object that can be dynamically opened and closed
    
    * Animation for Scissors
    
    * ARMTEX is removed from aramanim
    
    * subtleties with the numpy library
    
    * ARMTEX is removed
    
    * renamed to Problem10107
    
    * Imported missing stuff
    
    * Changes in code style
    
    * Այժմ to Now
    
    * subtleties with the numpy library
    
    * minor changes
    
    * Stopwatch object added
    
    * minor changes
    
    * Animations for Stopwatch added
    
    * stopwatch SVGs
    
    Co-authored-by: aram <arakeljan@gmail.com>

---
+++ lib/aramanim.py
@@ -0,0 +1,213 @@
+from manim import UP, DOWN, LEFT, RIGHT, ORIGIN, OUT, PI, DEFAULT_STROKE_WIDTH
+from manim import VMobject, VGroup, Line
+from manim import TexTemplate, MathTex
+from manim import FadeIn, FadeOut, Animation, Scene
+from manim import always_redraw
+import numpy as np
+from numpy import linalg as la
+from objects import Scissors, Stopwatch
+
+# np վեկտորների համար ՛մեթոդներ՛
+def length(array):
+    return la.norm(array)
+
+def normalized_vector(array):
+    array = np.array(array)
+    return (1/length(array)) * array
+
+def rotate2angle(array, alpha):
+    array = np.array(array)
+    rotation__matrix = [
+        [np.cos(alpha), -np.sin(alpha), 0],
+        [np.sin(alpha), np.cos(alpha), 0],
+        [0, 0, 1],
+    ]
+    return np.matmul(rotation__matrix, array)
+
+def normal(array):
+    array = np.array(array)
+    return normalized_vector(rotate2angle(array, PI/2))
+
+# հայերեն լեզվով LaTeX աշխատացնելու համար
+XELATEX_preamble = r"""
+\usepackage{tikz}
+\usepackage{fontspec}
+\setmainfont{DejaVu Serif}
+\usepackage{amsfonts,amssymb,amsthm,mathtools}
+\usepackage{amsmath}
+\usepackage{upgreek}
+\usepackage{mathrsfs}
+"""
+XELATEX = TexTemplate('xelatex', '.pdf', preamble=XELATEX_preamble)
+
+# Մասերով խնդրի համար
+DEFAULT_EDGE_HEIGHT = 0.2
+DEFAULT_LABEL_FONT_SIZE = 50
+
+class Segment(VGroup):
+    def __init__(
+            self,
+            start = 0.5 * LEFT,
+            end  = 0.5 * RIGHT,
+            label = None,
+            label_font_size = DEFAULT_LABEL_FONT_SIZE,
+            **kwargs
+        ):
+        start = np.array(start)
+        end = np.array(end)
+        normal_direction = normal(end-start)
+        if 'stroke_width' in kwargs:
+            stroke_width = kwargs['stroke_width']
+        else:
+            stroke_width = DEFAULT_STROKE_WIDTH
+        if 'edge_hight' in kwargs:
+            edge_hight = kwargs['edge_hight']
+            del kwargs['edge_hight']
+        else:
+            edge_hight = DEFAULT_EDGE_HEIGHT        
+        self.line = Line(start, end, buff=0, **kwargs)
+        self.left_edge = always_redraw(
+            lambda: Line(
+                ORIGIN,
+                edge_hight * stroke_width / 5 * normal_direction,
+                **kwargs
+            ).move_to(self.line.get_left())
+        )
+        self.right_edge = always_redraw(
+            lambda: Line(
+                ORIGIN,
+                edge_hight * stroke_width / 5 * normal_direction,
+                **kwargs
+            ).move_to(self.line.get_right())
+        )
+        super().__init__(self.line, self.left_edge, self.right_edge)
+        self.label = VMobject()
+        if label != None:
+            label = MathTex(label, font_size = label_font_size)
+            self.set_label(label)
+
+    def set_label(self, label):
+        self.label.become(label)
+        self.update_label_pos()
+
+    def update_label_pos(self, buff = 0.05, direction = UP):
+        self.label.next_to(self, direction, buff)
+        return self.label
+
+# մկրատի անիմացիաներ (Scissors)
+class CutIn(FadeIn):
+    def __init__(self, mobject: Scissors, **kwargs):
+        super().__init__(mobject, **kwargs)
+    def interpolate(self, alpha: float):
+        if self.rate_func(alpha) < 0.5:
+            angle = (PI / 6) * self.rate_func(alpha)
+            self.mobject.set_opacity(2*self.rate_func(alpha))
+        else:
+            angle = (PI / 6) * (1-self.rate_func(alpha))
+        self.mobject.become(self.starting_mobject)
+        self.mobject.open(angle)
+        self.mobject.shift(0.5*UP*self.rate_func(alpha))
+    def clean_up_from_scene(self, scene: Scene) -> None:
+        scene.add(self.mobject)
+
+class CutOut(FadeOut):
+    def __init__(self, mobject: Scissors, **kwargs):
+        super().__init__(mobject, **kwargs)
+    def interpolate(self, alpha: float):
+        if self.rate_func(alpha) > 0.5:
+            self.mobject.set_opacity(2*(1-self.rate_func(alpha)))
+        angle = (PI / 9) * self.rate_func(alpha)
+        self.mobject.become(self.starting_mobject)
+        self.mobject.open(angle)
+        self.mobject.shift(0.5*DOWN*self.rate_func(alpha))
+    def clean_up_from_scene(self, scene: Scene) -> None:
+        scene.remove(self.mobject)
+
+# ժամացույցի անիմացիա
+class Run(Animation):
+    def __init__(
+        self,
+        mobject: Stopwatch,
+        speed=1,
+        run_time =1,
+        dirt = 1,
+        **kwargs
+    ) -> None:
+        super().__init__(
+            mobject,
+            run_time = run_time,
+            **kwargs
+        )
+        self.radians = 2 * PI * speed * run_time / 60 * dirt
+        self.time_passed = speed * run_time * dirt
+        self.about_point = mobject.stopwatch.get_center()
+        self.shift_resetter = 0.3 * (
+            np.array(mobject.stopwatch_resetter.get_center())
+            - np.array(mobject.stopwatch.get_edge_center(UP))
+        )
+
+    def interpolate(self, alpha: float) -> None:
+        if alpha < 0.1:
+            self.mobject.stopwatch_resetter.move_to(
+                self.starting_mobject.stopwatch_resetter.get_center()
+                - (10*alpha) * self.shift_resetter
+            )
+        if alpha >= 0.9 and alpha <=1:
+            self.mobject.stopwatch_resetter.move_to(
+                self.starting_mobject.stopwatch_resetter.get_center()
+                - (10*(1-alpha)) * self.shift_resetter
+            )
+        self.mobject.stopwatch_arrow.become(
+            self.starting_mobject.stopwatch_arrow
+        )
+        self.mobject.stopwatch_arrow.rotate(
+            -self.rate_func(alpha) * self.radians,
+            axis=OUT,
+            about_point=self.about_point
+        )
+        self.mobject.time.set_value(
+            self.starting_mobject.time.get_value()
+            + self.rate_func(alpha) * self.time_passed
+        )
+
+class Reset(Animation):
+    def __init__(
+        self,
+        mobject: Stopwatch,
+        **kwargs
+    ) -> None:
+        super().__init__(mobject, run_time=0.4, **kwargs)
+        self.shift_resetter = 0.3 * (
+            np.array(mobject.stopwatch_resetter.get_center())
+            - np.array(mobject.stopwatch.get_edge_center(UP))
+        )
+        self.radians = 2*PI*(mobject.time.get_value()%60)/60
+        self.about_point = mobject.stopwatch.get_center()
+    def interpolate(self, alpha):
+        if alpha < 0.25:
+            self.mobject.stopwatch_resetter.move_to(
+                self.starting_mobject.stopwatch_resetter.get_center()
+                - self.rate_func(4*alpha) * self.shift_resetter
+            )
+        if alpha >= 0.25 and alpha < 0.5:
+            self.mobject.stopwatch_resetter.move_to(
+                self.starting_mobject.stopwatch_resetter.get_center()
+                - self.rate_func(4*(0.5-alpha)) * self.shift_resetter
+            )
+        if alpha >= 0.5 and alpha < 0.75:
+            self.mobject.stopwatch_resetter.move_to(
+                self.starting_mobject.stopwatch_resetter.get_center()
+                - self.rate_func(4*(0.75-alpha)) * self.shift_resetter
+            )
+        if alpha >= 0.75:
+            self.mobject.stopwatch_resetter.move_to(
+                self.starting_mobject.stopwatch_resetter.get_center()
+                - self.rate_func(4*(1-alpha)) * self.shift_resetter
+            )
+        self.mobject.stopwatch_arrow.become(self.starting_mobject.stopwatch_arrow)
+        self.mobject.stopwatch_arrow.rotate(
+            self.rate_func(alpha) * self.radians,
+            axis=OUT,
+            about_point=self.about_point
+        )
+        self.mobject.time.set_value(0)
