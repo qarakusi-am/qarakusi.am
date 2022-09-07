d59011ce1672d3676eda2557b00ea11156f788f8
aram <arakeljan@gmail.com>
2022-09-07 12:04:06 +0400

    dir fix

--- problems/enumerated/problem_10382/english_text.py
+++
@@ -1,4 +0,0 @@
-arshak_str = "Arshak's money"
-levon_str = "Levon's money"
-toy_str = "Toy price"
-missing_str = "missing amount"
--- problems/enumerated/problem_10382/text.py
+++
@@ -1,9 +0,0 @@
-from constants import LanguageConfig
-
-if LanguageConfig.is_armenian():
-    from .armenian_text import arshak_str, levon_str, toy_str, missing_str
-else:
-    from .english_text import arshak_str, levon_str, toy_str, missing_str
-
-
-
