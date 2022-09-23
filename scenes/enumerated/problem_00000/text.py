da34096ac378b9e21ccc12112933f9f2878e9f08
aram <arakeljan@gmail.com>
2022-09-23 12:21:17 +0400

    added some text

--- scenes/enumerated/problem_00000/text.py
+++ scenes/enumerated/problem_00000/text.py
@@ -1,6 +1,6 @@
 from constants import LanguageConfig
 
 if LanguageConfig.is_armenian():
-    from .armenian_text import coordinate_text, number_line_text
+    from .armenian_text import coordinate_text, number_line_text, step_number_text, left_ruler_text, rigth_ruler_text
 else:
-    from .english_text import coordinate_text, number_line_text
+    from .english_text import coordinate_text, number_line_text, step_number_text, left_ruler_text, rigth_ruler_text
