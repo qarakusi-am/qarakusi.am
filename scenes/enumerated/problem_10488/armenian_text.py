30f7430603ecfa3b402fcae1a16caed6535d8b10
aram <arakeljan@gmail.com>
2022-09-26 13:03:34 +0400

    added tests for problem 10488

---
+++ scenes/enumerated/problem_10488/armenian_text.py
@@ -0,0 +1,8 @@
+condition_1_string = r"\textrm{պայման } $1$"
+condition_2_string = r"\textrm{պայման } $2$"
+three_times_more_list = [
+    r"$2$ \textrm{րդ լողավազանում ջուրը } $3$",
+    r"\textrm{անգամ շատ է քան առաջինում}"]
+in_total_list = [
+    r"$60$ \textrm{և} $40$ \textrm{լիտր ջուր ավելացնելուց հետո, }",
+    r"\textrm{լողավազաններում ընդհամենը} $600$ \textrm{լիտր ջուր էր}"]
