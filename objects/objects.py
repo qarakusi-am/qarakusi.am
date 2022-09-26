ef922b58f1c06f50b6c7a856a374aaac05fcc145
Aram Arakelyan <99004923+a-arakelian@users.noreply.github.com>
2022-09-26 10:19:24 +0400

    Terrain  (#57)
    * added trains objects
    
    * folder rename
    
    * Train class added
    
    * code style changes
    
    * code style changes
    
    * SVG object added
    
    Co-authored-by: aram <arakeljan@gmail.com>

--- objects/objects.py
+++ objects/objects.py
@@ -701,3 +701,14 @@ class Stopwatch(VGroup):
             axis=OUT,
             about_point=self.stopwatch.get_center()
         )
+
+class Train(VGroup):
+    def __init__(self, with_locomotive = True, passenger = True, number_of_rolling_cars = 2):
+        n = number_of_rolling_cars
+        path_to_cars = path_to_SVG / 'train' / 'passenger_car.svg' if passenger else path_to_SVG / 'train' / 'freight_car.svg'
+        path_to_locomotive = path_to_SVG / 'train' / 'locomotive.svg'
+        vmobjects = [SVGMobject(path_to_cars) for _ in range(n)]
+        if with_locomotive:
+            vmobjects.append(SVGMobject(path_to_locomotive))
+        super().__init__(*vmobjects)
+        self.arrange(RIGHT, aligned_edge=DOWN)
