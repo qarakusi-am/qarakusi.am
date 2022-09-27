b464553ae5bdb45db9cc144ce14e7cdc76968b03
zidder <zidder@hilearn.io>
2022-09-28 00:27:56 +0400

    Add a separate workflow for full run once a week

--- manim_test.py
+++ manim_test.py
@@ -1,10 +1,22 @@
 import os
 import subprocess
 from pathlib import Path
+from argparse import ArgumentParser
 
 
-def main():
+def parse_args():
+    parser = ArgumentParser(
+        description='run all problems in scenes/enumerated'
+    )
+    parser.add_argument('--last-screen', action='store_true')
+    return parser.parse_args()
+
+
+def main(args):
     enumerated_problems = Path('scenes/enumerated')
+    extra_args = []
+    if args.last_screen:
+        extra_args.append('-s')
     for path in enumerated_problems.glob('problem_*'):
         index = path.name[len('problem_'):]
         if not all(i.isdigit() for i in index):
@@ -12,9 +24,9 @@ def main():
             continue
         for language in ['armenian', 'english']:
             subprocess.run(['python', 'qarakusi.am.py', index,
-                            '-ql', '--language', language],
+                            '-ql', '--language', language] + extra_args,
                             env=os.environ)
 
 
 if __name__ == '__main__':
-    main()
+    main(parse_args())
