9a6413b8fb53d6d8699fe1dbf2c9d029a378c444
aram <arakeljan@gmail.com>
2022-09-29 16:09:16 +0400

    Changes in english text

--- scenes/enumerated/problem_10488/english_text.py
+++ scenes/enumerated/problem_10488/english_text.py
@@ -1,8 +1,8 @@
 condition_1_string = r"condition $1$"
 condition_2_string = r"condition $2$"
 three_times_more_list = [
-    r"the $2$ nd water in the pool is $3$",
+    r"the water in the $2$ nd pool $3$",
     r"times more than in the first"]
 in_total_list = [
     r"After adding $60$ and $40$ liters of water, there ",
-    r"were only $600$ liters of water in the pools"]
+    r"were in total $600$ liters of water in the pools"]
