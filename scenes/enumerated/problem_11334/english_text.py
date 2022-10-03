8364dd04a4a47988ab37574d2219b8958ae0cabb
valentinnaa <112026828+valentinnaa@users.noreply.github.com>
2022-10-03 11:56:17 +0400

    changing text's font_size=40 from 48

--- scenes/enumerated/problem_11334/english_text.py
+++ scenes/enumerated/problem_11334/english_text.py
@@ -1,8 +1,8 @@
 from manim import Tex
 
-youngest = Tex('The youngest')
-second = Tex('The second', ' child is $3$ years older than the youngest.')
-third = Tex('The middle', ' child is $3$ years older than the second.')
-fourth = Tex('The fourth', ' child is $3$ years older than the middle.')
-fifth = Tex('The oldest', ' child is also $3$ years older than the fourth.')
-fifth_2 = Tex('The oldest', 'child is $4$ times older than the youngest.')
+youngest = Tex('The youngest', font_size=40)
+second = Tex('The second', ' child is $3$ years older than the youngest.', font_size=40)
+third = Tex('The middle', ' child is $3$ years older than the second.', font_size=40)
+fourth = Tex('The fourth', ' child is $3$ years older than the middle.', font_size=40)
+fifth = Tex('The oldest', ' child is also $3$ years older than the fourth.', font_size=40)
+fifth_2 = Tex('The oldest', 'child is $4$ times older than the youngest.', font_size=40)
