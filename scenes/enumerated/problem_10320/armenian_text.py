864fba4badc37fd0ac138cfc801aa7fc7e5eb428
Gor <simonyan.gor.397@gmail.com>
2022-10-24 15:07:52 +0400

    վերջնական

--- scenes/enumerated/problem_10320/armenian_text.py
+++ scenes/enumerated/problem_10320/armenian_text.py
@@ -1,15 +1,8 @@
 task_number_string = "Խ. 10320"
 Arsen_string = "Արսեն"
 road_length_string = r"605\text{կմ}"
-<<<<<<< HEAD
 first_part_of_road_time_string = r"4\text{ ժամ}"
 second_part_of_road_time_string = r"3\text{ ժամ}"
 segment_15_string = "15"
 hour_string = r"\text{ ժամ}"
-=======
-first_part_of_road_time_string = r"4\text{ժ}"
-second_part_of_road_time_string = r"3\text{ժ}"
-segment_15_string = "15"
-hour_string = r"\text{ժ}"
->>>>>>> 9e4ee688748b7b6215344e1e927d3c0dc6124a38
 km = r"\text{կմ}"
