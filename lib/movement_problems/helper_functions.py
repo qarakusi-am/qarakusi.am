c344edbb76ca1b916b2a6a4207f9230ba947129f
Hovhannes Yepremyan <61244745+AlternativeFuture@users.noreply.github.com>
2023-03-02 13:04:02 +0400

    Problem 12695 (#150)
    * problem_12695
    
    * final
    
    * final
    
    * final final
    
    * add docstring to movment package and requested changes
    
    * docs: fix some docstrings

---
+++ lib/movement_problems/helper_functions.py
@@ -0,0 +1,29 @@
+from manim import MathTex
+from manim import BraceBetweenPoints
+from manim import rate_functions
+from manim import DOWN, UP, LEFT, RIGHT
+
+
+def down_brace_with_text(text: str, points: tuple, brace_shift=(0.1 * UP), text_shift=(0.7 * DOWN)):
+    """Parameters:
+           text: str
+           points: tuple
+           brace_shift: step
+           text_shift: step
+       Returns:
+           brace_with_text (tuple): brace with text"""
+    brace = BraceBetweenPoints(points[0], points[1], direction=DOWN).shift(brace_shift)
+    brace_text = MathTex(text).move_to(brace).shift(text_shift).scale(1.2)
+    return brace, brace_text
+
+
+def obj_movement(obj, move_to_point, run_time=1):
+    """Parameters:
+           obj: object
+           move_to_point: point to where object must be moved
+           run_time: execution time
+       Returns:
+           obj_movement (obj): moved object"""
+    res = obj.animate(rate_func=rate_functions.linear, run_time=run_time).move_to(
+        move_to_point, aligned_edge=RIGHT).shift(0.05 * LEFT + 0.3 * UP)
+    return res
