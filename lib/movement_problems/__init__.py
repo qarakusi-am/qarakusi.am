ae7f48b88a1b6e70236fbb3c30ac39dde9c51b59
Hovhannes Yepremyan <61244745+AlternativeFuture@users.noreply.github.com>
2023-03-09 13:57:50 +0400

    Problem 12699 (#171)

--- lib/movement_problems/__init__.py
+++ lib/movement_problems/__init__.py
@@ -1,5 +1,5 @@
 from lib.movement_problems.coordinate import Coordinate
-from lib.movement_problems.helper_functions import obj_movement, down_brace_with_text
+from lib.movement_problems.helper_functions import obj_movement, down_brace_with_text, up_brace_with_text
 from lib.movement_problems.custom_movement_scene import CustomMovementScene
 
-__all__ = ['down_brace_with_text', 'obj_movement', 'Coordinate', 'CustomMovementScene']
+__all__ = ['down_brace_with_text', 'obj_movement', 'Coordinate', 'CustomMovementScene', 'up_brace_with_text']
--- lib/movement_problems/helper_functions.py
+++ lib/movement_problems/helper_functions.py
@@ -2,18 +2,33 @@ from manim import MathTex
 from manim import BraceBetweenPoints
 from manim import rate_functions
 from manim import DOWN, UP, LEFT, RIGHT
+from manim import WHITE
 
 
-def down_brace_with_text(text: str, points: tuple, brace_shift=(0.1 * UP), text_shift=(0.7 * DOWN)):
+def down_brace_with_text(text: str, points: tuple, color=WHITE, brace_shift=(0.1 * UP), text_shift=(0.7 * DOWN)):
     """Parameters:
            text: str
            points: tuple
+           color: str
            brace_shift: step
            text_shift: step
        Returns:
-           brace_with_text (tuple): brace with text"""
+           down_brace_with_text (tuple): down_brace_with_text"""
     brace = BraceBetweenPoints(points[0], points[1], direction=DOWN).shift(brace_shift)
-    brace_text = MathTex(text).move_to(brace).shift(text_shift).scale(1.2)
+    brace_text = MathTex(text, color=color).move_to(brace).shift(text_shift).scale(1.2)
+    return brace, brace_text
+
+def up_brace_with_text(text: str, points: tuple, color=WHITE, brace_shift=(0.1 * DOWN), text_shift=(0.5 * UP)):
+    """Parameters:
+           text: str
+           points: tuple
+           color: str
+           brace_shift: step
+           text_shift: step
+       Returns:
+           up_brace_with_text (tuple): up_brace_with_text"""
+    brace = BraceBetweenPoints(points[0], points[1], direction=UP).shift(brace_shift)
+    brace_text = MathTex(text, color=color).move_to(brace).shift(text_shift).scale(1.2)
     return brace, brace_text
 
 
