6387cf5e92c8323be6e319bb126bca83057a241f
alternativefuture <yepremyanhovhannes11@gmail.com>
2023-03-09 14:16:38 +0400

    Final

--- scenes/enumerated/problem_12187/text.py
+++ scenes/enumerated/problem_12187/text.py
@@ -4,3 +4,12 @@ if LanguageConfig.is_armenian():
     from .armenian_text import *
 else:
     from .english_text import *
+
+
+CONDITION_4 = r"\text{1,2,3,4,5,6,7,8,9}"
+CONDITION_6 = r"\text{9 + 9} < {23}"
+CONDITION_8 = r"\text{9+9+9} > {23}"
+PLUS = r"\text{ + }"
+EQUAL_23 = r"\text{ = 23 }"
+NINE = r"9"
+SOLUTION_PART_1 = r"\text{23 - 9 - 9 = 5 }"
