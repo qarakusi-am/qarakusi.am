e71291b7f6d32d614a48acd9ffacd36dc147f5d8
alternativefuture <yepremyanhovhannes11@gmail.com>
2023-03-14 19:51:47 +0400

    problem_12706

---
+++ scenes/enumerated/problem_12706/english_text.py
@@ -0,0 +1,4 @@
+TASK_NUMBER_STR = "P. 12706"
+CONDITION_1_0 = r"\text{Delete one digit to get}"
+CONDITION_1_1 = r"\text{the largest possible number}"
+CONDITION_5 = r"\text{the number will be 11 digits long}"
