06262b9d9352573c223ac86122ef25069e1ea638
Gor397 <simonyan.gor.397@gmail.com>
2023-03-17 22:29:12 +0400

    fixed something in text

--- scenes/enumerated/problem_12723/armenian_text.py
+++ scenes/enumerated/problem_12723/armenian_text.py
@@ -1,6 +1,6 @@
 task_number_str = "Խ. 12723"
 air_mass_str = " գրամ օդ"
-molecules_in_one_gram_str = r"$1$ գրամ օդում $\rightarrow 2 \cdot 10^{22}$ մոլեկուլ"
+molecules_in_one_gram_str = r"$1$ գրամ օդում կա $2 \cdot 10^{22}$ մոլեկուլ"
 molecules_in_ball_str = "կա ", "$450$", " $\cdot$ ", "$2$", " $\cdot$ ", "$10^{22}$", " մոլեկուլ"
 hat_str = " հատ"
 
