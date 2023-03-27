eb26ead986eaa02da36b8a3bc63ebdf60b1d9dc2
LilitM <lilit.margaryan@naghashyan.com>
2023-03-27 14:11:36 +0400

    initial problem 12722 commit

--- scenes/enumerated/problem_12722/text.py
+++ scenes/enumerated/problem_12722/text.py
@@ -4,3 +4,9 @@ if LanguageConfig.is_armenian():
     from .armenian_text import *
 else:
     from .english_text import *
+__all__ = [
+    "taskNumberString",
+    "for_what_value_k",
+    "is_a_solution"
+]
+
