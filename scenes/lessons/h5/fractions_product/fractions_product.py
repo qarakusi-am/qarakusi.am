fe927513cae4d91d46f2db314655461aeb2d509f
alternativefuture <yepremyanhovhannes11@gmail.com>
2023-04-03 16:06:50 +0400

    WIP

--- scenes/lessons/h5/fractions_product/fractions_product.py
+++ scenes/lessons/h5/fractions_product/fractions_product.py
@@ -1,19 +1,17 @@
 from qarakusiscene import QarakusiScene
 from . import text
 
-from manim import UP, DOWN, UL, DR
+from manim import UP, DOWN, UL, DR, LEFT, RIGHT
 from manim import FadeOut, Write
-from manim import VGroup, Line, MathTex
-from manim import YELLOW
+from manim import VGroup, Line, MathTex, Arrow, Group
+from manim import YELLOW, ORANGE, BLUE_C, WHITE, RED
 
 
 class FractionsProduct(QarakusiScene):
     """Հ5 – կոտորակների արտադրյալը"""
     def construct(self):
-        condition_point = [-2, 3, 0]
+        condition_point = [-1.5, 3, 0]
         MathTex.set_default(font_size=60)
-        self.add_task_number(text=text.TASK_NUMBER_STR)
-        # self.add_plane()
 
         # -------------------------- Point 1 ------------------------------- #
         condition_1 = MathTex(*text.CONDITION_1).next_to(condition_point)
@@ -22,25 +20,32 @@ class FractionsProduct(QarakusiScene):
 
         # -------------------------- Point 2 ------------------------------- #
         condition_1_0 = condition_1[0].copy()
-        self.play(condition_1_0.animate.next_to(condition_1[0], DOWN))
-        condition_2 = MathTex(*text.CONDITION_2).next_to(condition_1_0)
+        arrow_1 = Arrow(condition_1[0].get_critical_point(LEFT), [-3.8, 0.7, 0], color=ORANGE)
+        self.play(Write(arrow_1))
+        self.play(condition_1_0.animate(run_time=2).set_color(ORANGE).shift(3 * DOWN + 5.5 * LEFT))
+        condition_2 = MathTex(*text.CONDITION_2, color=ORANGE).next_to(condition_1_0)
         self.play(Write(condition_2))
         self.wait()
 
         # -------------------------- Point 3 ------------------------------- #
-        self.play(condition_2[1:].animate.next_to(condition_1), FadeOut(condition_2[0]), FadeOut(condition_1_0))
-        self.wait()
 
         # -------------------------- Point 4 ------------------------------- #
         condition_1_2 = condition_1[2].copy()
-        self.play(condition_1_2.animate.next_to(condition_1[0], DOWN))
-        condition_3 = MathTex(*text.CONDITION_3).next_to(condition_1_2)
+        arrow_2 = Arrow(condition_1[2].get_critical_point(RIGHT), [3.8, 0.7, 0], color=BLUE_C).set_z_index(-1)
+        self.play(Write(arrow_2))
+        self.play(condition_1_2.animate(run_time=2).set_color(BLUE_C).shift(3 * DOWN))
+        condition_3 = MathTex(*text.CONDITION_3, color=BLUE_C).next_to(condition_1_2)
         self.play(Write(condition_3))
         self.wait()
 
-        # -------------------------- Point 5 ------------------------------- #
-        cdot = condition_1[1].copy().next_to(condition_2[2])
-        self.play(condition_3[1].animate.next_to(cdot), FadeOut(condition_1_2), FadeOut(condition_3[0]))
+        # -------------------------- Point 3 ------------------------------- #
+        equal = condition_2[1].copy().next_to(condition_1).set_color(WHITE)
+        self.play(Write(equal))
+        cdot = condition_1[1].copy().next_to(equal).shift(0.8 * RIGHT)
+        self.play(condition_2[2].animate(run_time=2).next_to(equal),
+                  FadeOut(Group(condition_2[:2], condition_1_0, arrow_1)),
+                  condition_3[1].animate(run_time=2.2).next_to(cdot),
+                  FadeOut(Group(condition_1_2, condition_3[0], arrow_2)))
         self.play(Write(cdot))
         self.wait()
 
@@ -70,30 +75,35 @@ class FractionsProduct(QarakusiScene):
 
         # -------------------------- Point 10 ------------------------------- #
         option_2 = solution_group_1.copy() + condition_5[0].copy()
-        self.play(option_2.animate(run_time=1.5).next_to(condition_6))
+        self.play(option_2.animate(run_time=2).next_to(condition_6))
         self.wait()
 
         # -------------------------- Point 11 ------------------------------- #
+        nums = MathTex(*text.NUMS).scale(0.6).set_color(YELLOW)
         line_1 = Line(option_2[3][1:3].get_critical_point(UL),
                       option_2[3][1:3].get_critical_point(DR),
                       stroke_width=5,
-                      color=YELLOW)
+                      color=RED)
         line_2 = Line(option_2[3][10:].get_critical_point(UL),
                       option_2[3][10:].get_critical_point(DR),
                       stroke_width=5,
-                      color=YELLOW)
+                      color=RED)
         self.play(Write(VGroup(line_1, line_2)))
+        self.play(Write(VGroup(nums[2].next_to(option_2[3][1:3]).shift(0.2 * UP + 1.05 * LEFT),
+                               nums[0].next_to(option_2[3][10:]).shift(0.2 * UP + 1.05 * LEFT))))
         self.wait()
 
         line_3 = Line(option_2[3][4:6].get_critical_point(UL),
                       option_2[3][4:6].get_critical_point(DR),
                       stroke_width=5,
-                      color=YELLOW)
+                      color=RED)
         line_4 = Line(option_2[3][7:9].get_critical_point(UL),
                       option_2[3][7:9].get_critical_point(DR),
                       stroke_width=5,
-                      color=YELLOW)
+                      color=RED)
         self.play(Write(VGroup(line_3, line_4)))
+        self.play(Write(VGroup(nums[3].next_to(option_2[3][4:6]).shift(0.2 * UP + 1.05 * LEFT),
+                               nums[1].next_to(option_2[3][7:9]).shift(0.2 * UP + 1.05 * LEFT))))
         self.wait()
 
         # -------------------------- Point 12 ------------------------------- #
@@ -106,7 +116,7 @@ class FractionsProduct(QarakusiScene):
         self.wait()
 
         # -------------------------- Point 13 ------------------------------- #
-        option_2_all = VGroup(condition_6, option_2, condition_7, line_1, line_2, line_3, line_4)
+        option_2_all = VGroup(condition_6, option_2, condition_7, line_1, line_2, line_3, line_4, nums)
         self.play(option_2_all.animate(run_time=1.5).shift(1.8 * DOWN))
         self.wait()
 
@@ -117,26 +127,31 @@ class FractionsProduct(QarakusiScene):
         self.wait()
 
         # -------------------------- Point 14 ------------------------------- #
+        nums_1 = nums.copy()
         line_5 = Line(condition_8_1[0][:2].get_critical_point(UL),
                       condition_8_1[0][:2].get_critical_point(DR),
                       stroke_width=5,
-                      color=YELLOW)
+                      color=RED)
         line_6 = Line(condition_8_1[2][3:].get_critical_point(UL),
                       condition_8_1[2][3:].get_critical_point(DR),
                       stroke_width=5,
-                      color=YELLOW)
+                      color=RED)
         self.play(Write(VGroup(line_5, line_6)))
+        self.play(Write(VGroup(nums_1[2].next_to(condition_8_1[0][:2]).shift(0.2 * UP + 1.05 * LEFT),
+                               nums_1[0].next_to(condition_8_1[2][3:]).shift(0.2 * UP + 1.05 * LEFT))))
         self.wait()
 
         line_7 = Line(condition_8_1[0][3:].get_critical_point(UL),
                       condition_8_1[0][3:].get_critical_point(DR),
                       stroke_width=5,
-                      color=YELLOW)
+                      color=RED)
         line_8 = Line(condition_8_1[2][:2].get_critical_point(UL),
                       condition_8_1[2][:2].get_critical_point(DR),
                       stroke_width=5,
-                      color=YELLOW)
+                      color=RED)
         self.play(Write(VGroup(line_7, line_8)))
+        self.play(Write(VGroup(nums_1[1].next_to(condition_8_1[0][3:]).shift(0.2 * UP + 1.05 * LEFT),
+                               nums_1[3].next_to(condition_8_1[2][:2]).shift(0.2 * UP + 1.05 * LEFT))))
         self.wait()
 
         # -------------------------- Point 15 ------------------------------- #
