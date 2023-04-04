cf75c49f6a0cb81e721dbf4622274fe8e1b36fab
alternativefuture <yepremyanhovhannes11@gmail.com>
2023-04-04 11:19:17 +0400

    Problem12203

--- scenes/enumerated/problem_12203/english_text.py
+++ scenes/enumerated/problem_12203/english_text.py
@@ -8,8 +8,8 @@ CONDITION_1 = r"\text{3 days, 6 hours per day}"
 CONDITION_2 = r"\text{2 days, 7 hours per day}"
 CONDITION_3 = r"\text{168 ha}"
 CONDITION_45 = [r"\text{In total, in }", r"\text{  days }" , r"\text{ hours }"]
-CONDITION_6 = r"\text{168 ha = 168 } \cdot \text{ 100 are = 16800 are}"
-CONDITION_7 = [r"\text{16800 are : 32 = }", r"\text{525 are}"]
+CONDITION_6 = r"\text{168 ha = 168 } \cdot \text{ 100 ares = 16800 ares}"
+CONDITION_7 = [r"\text{16800 ares : 32 = }", r"\text{525 ares}"]
 CONDITION_8 = r"\text{1 hour}"
 
 addition_to_condition_buff = 2.5
