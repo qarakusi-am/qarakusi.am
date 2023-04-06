6df95a619be3e05c64105b6795fd076887b9ec84
Gor <74136975+Gor397@users.noreply.github.com>
2023-04-06 22:24:02 +0400

    Problem 12723 (#185)
    * added new svgs
    
    * added move_to argument to ModifyFormula()
    
    * added __all__ into text.py of 12707
    
    * added new problem
    
    * added english text shift
    
    * fixed something in text
    
    * fixed english
    
    * Update scenes/enumerated/problem_12723/problem_12723.py
    
    Co-authored-by: zidder <hambardzumyanarsen99@gmail.com>
    
    * added new svgs
    
    ---------
    
    Co-authored-by: zidder <hambardzumyanarsen99@gmail.com>

--- objects/objects.py
+++ objects/objects.py
@@ -284,6 +284,15 @@ svg_defaults.add_prefix_default(
     {'folder': path_to_SVG / 'colored_books',
      'stroke_width': 1}
 )
+svg_defaults.add_prefix_default(
+    'karkandak_',
+    {'folder': path_to_SVG / 'food'}
+)
+svg_defaults.add_prefix_default(
+    'football_',
+    {'folder': path_to_SVG / 'balls'}
+)
+
 
 
 class SimpleSVGMobject(VMobject):
---
+++ scenes/enumerated/problem_12723/english_text.py
@@ -0,0 +1,9 @@
+from manim import DOWN, LEFT
+
+task_number_str = "P. 12723"
+air_mass_str = " grams of air"
+molecules_in_one_gram_str = r"There are $2 \cdot 10^{22}$ molecules in $1$ gram of air"
+molecules_in_ball_str = "There is ", "$450$", " $\cdot$ ", "$2$", " $\cdot$ ", "$10^{22}$", " molecules"
+hat_str = " times"
+
+molecules_in_ball_shift = DOWN*1.8+LEFT*3.5
