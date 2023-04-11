b62e4e928d3a90b32a3afe4d15ba7b7541e1e9e4
Gor397 <simonyan.gor.397@gmail.com>
2023-04-12 00:25:12 +0400

    fixed english

--- scenes/enumerated/problem_12737/english_text.py
+++ scenes/enumerated/problem_12737/english_text.py
@@ -1,4 +1,4 @@
 task_number_str = "P. 12737"
 task_str = "greatest common divisor\\\\and\\\\least common multiple\\\\of $60$ and $135$"
 new_task_str = "Calculate $(60,135)$ and $[60,135]$"
-tex1_str = "There isn't $2$"
+tex1_str = "There isn't factor $2$"
