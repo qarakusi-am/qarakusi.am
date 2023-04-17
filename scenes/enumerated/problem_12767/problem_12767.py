b4b27dc684340e407cb8069102a3ad4b5f3a2711
alternativefuture <yepremyanhovhannes11@gmail.com>
2023-04-17 18:20:03 +0400

    WIP

--- scenes/enumerated/problem_12767/problem_12767.py
+++ scenes/enumerated/problem_12767/problem_12767.py
@@ -3,7 +3,7 @@ from manim import FadeIn, FadeOut, Write, Create
 from manim import AnimationGroup, Group, VGroup
 from manim import MathTex, DashedLine, Rectangle, Line, BraceBetweenPoints
 from manim import rate_functions, ReplacementTransform
-from manim import PURE_RED, YELLOW
+from manim import PURE_RED, YELLOW, ORANGE, BLUE
 
 from movement_problems import Coordinate
 from constants import DEFAULT_SEGMENT_STROKE_WIDTH
@@ -28,7 +28,7 @@ class Problem12767(QarakusiScene):
 
         MathTex.set_default(font_size=60)
         self.add_task_number(text=text.TASK_NUMBER_STR)
-        self.add_plane()
+        # self.add_plane()
 
         # -------------------------- Point 1 ------------------------------- #
         # show rectangle
@@ -48,28 +48,40 @@ class Problem12767(QarakusiScene):
 
         # -------------------------- Point 2 ------------------------------- #
         length_sides = VGroup(*[MathTex(text.LENGTH_SIDE).next_to(*pos) for pos in ((left_line, LEFT), (right_line, RIGHT))])
-        self.play(Group(right_line, left_line).animate.set_color(YELLOW), FadeIn(*length_sides))
+        self.play(Group(right_line, left_line).animate.set_color(ORANGE),
+                  # FadeIn(*length_sides),
+                  Group(top_line, bottom_line).animate.set_color(BLUE))
         self.wait()
 
         # -------------------------- Point 3 ------------------------------- #
         rectangle_group_copy = rectangle_group.copy()
-        self.play(FadeIn(rectangle_group_copy))
-        self.wait()
+        # self.play(FadeIn(rectangle_group_copy))
+        # self.wait()
 
         # -------------------------- Point 4 ------------------------------- #
-        self.play(Group(perimeter, rectangle_group).animate.shift(4.1 * RIGHT),
-                  Group(length_sides, rectangle_group_copy).animate.shift(3 * LEFT))
+        self.play(Group(perimeter, rectangle_group).animate.shift(4.1 * RIGHT + 2 * DOWN),
+                  length_sides[0].copy().animate.shift(4.1 * RIGHT + 2 * DOWN),)
+                  # Group(length_sides, rectangle_group_copy).animate.shift(3 * LEFT))
+        Group(length_sides, rectangle_group_copy).shift(3 * LEFT)
         left_line_shift = (0.70 * DOWN)
         right_line_shift = (2.2 * DOWN + 5 * LEFT)
-        left_line_invisible = rectangle_group_copy[2].copy().shift(left_line_shift + 0.4 * UP).rotate(0.5 * PI)
-        right_line_invisible = rectangle_group_copy[3].copy().shift(right_line_shift + 0.4 * UP).rotate(0.5 * PI)
+        # left_line_invisible = rectangle_group_copy[2].copy().shift(left_line_shift + 0.4 * UP).rotate(0.5 * PI)
+        # right_line_invisible = rectangle_group_copy[3].copy().shift(right_line_shift + 0.4 * UP).rotate(0.5 * PI)
 
-        self.play(rectangle_group_copy[0].animate.shift(1 * UP + 1.25 * LEFT),
-                  rectangle_group_copy[1].animate.shift(2 * UP + 1.25 * LEFT),
-                  rectangle_group_copy[2].animate.shift(left_line_shift).rotate(0.5 * PI),
-                  rectangle_group_copy[3].animate.shift(right_line_shift).rotate(0.5 * PI),
-                  length_sides[0].animate.move_to(left_line_invisible),
-                  length_sides[1].animate.move_to(right_line_invisible))
+        rectangle_group_copy[0].shift(1 * UP + 1.25 * LEFT)
+        rectangle_group_copy[1].shift(2 * UP + 1.25 * LEFT)
+        rectangle_group_copy[2].shift(left_line_shift).rotate(0.5 * PI)
+        rectangle_group_copy[3].shift(right_line_shift).rotate(0.5 * PI)
+        length_sides[0].next_to(rectangle_group_copy[2], UP)
+        length_sides[1].next_to(rectangle_group_copy[3], UP)
+
+        # self.play(rectangle_group_copy[0].animate.shift(1 * UP + 1.25 * LEFT),
+        #           rectangle_group_copy[1].animate.shift(2 * UP + 1.25 * LEFT),
+        #           rectangle_group_copy[2].animate.shift(left_line_shift).rotate(0.5 * PI),
+        #           rectangle_group_copy[3].animate.shift(right_line_shift).rotate(0.5 * PI),
+        #           length_sides[0].animate.move_to(left_line_invisible),
+        #           length_sides[1].animate.move_to(right_line_invisible))
+        self.play(FadeIn(rectangle_group_copy), FadeIn(length_sides))
         self.wait()
 
         end_mark_pattern = SegmentEndmark(length=DEFAULT_SEGMENT_STROKE_WIDTH / 20).set_z_index(+1)
@@ -83,14 +95,13 @@ class Problem12767(QarakusiScene):
 
         self.play(
             Write(VGroup(*left_end_marks, *right_end_marks)),
-            perimeter.animate.shift(5.1 * LEFT),
+            perimeter.animate.shift(5.1 * LEFT + 2 * UP),
             ReplacementTransform(perimeter[0], brace))
         self.wait()
 
         # -------------------------- Point 5 ------------------------------- #
         self.play(brace.animate.scale(0.40).set_stroke_width(2.7).shift(1.5 * UP),
-                  perimeter[1].animate.shift(1.5 * UP),
-                  rectangle_group.animate.shift(2 * DOWN))
+                  perimeter[1].animate.shift(1.5 * UP))
 
         num_1 = MathTex(text.NUM_1).next_to(perimeter[1])
         self.play(Write(num_1))
