5c7e0034c1cdcd0b0cfc524bc60f025f23499bb2
LilitM <lilit.margaryan@naghashyan.com>
2023-04-19 16:07:58 +0400

    fixes

--- scenes/enumerated/problem_12754/problem_12754.py
+++ scenes/enumerated/problem_12754/problem_12754.py
@@ -5,7 +5,7 @@ from manim import YELLOW, BLUE, WHITE, ORANGE, GREEN
 from manim import RIGHT, LEFT, UP, DOWN
 from manim import MathTex, VGroup, MovingCameraScene
 
-FONT_SIZE = 50
+FONT_SIZE = 55
 RUN_TIME_SPEED = 2
 
 
@@ -15,15 +15,15 @@ class Problem12754(MovingCameraScene):
 
         coordinate_axes = Axes(
             x_range=[-4, 6, 1],
-            y_range=[-3, 10, 1],
-            x_length=7,
-            y_length=5,
+            y_range=[-3.3, 10, 1],
+            x_length=9,
+            y_length=6,
             axis_config={
                 "include_numbers": True,
-                "font_size": 35,
+                "font_size": 40,
             },
             tips=True,
-        ).shift(DOWN * 0.5)
+        ).shift(DOWN * 0.6)
         # graphs and respective formulas
         graph1 = coordinate_axes.plot(lambda t: 2 * t + 4,
                                       x_range = [-3, 5],
@@ -44,7 +44,7 @@ class Problem12754(MovingCameraScene):
         line_intersection_point1 = VGroup(
             coordinate_axes.get_vertical_line(coordinate_axes.c2p(1, 15, 0), color = BLUE),
             Dot(point = coordinate_axes.c2p(1, 6, 0), color = YELLOW),
-            MathTex("(x,", "2x + 4", ")").scale(0.7).next_to(coordinate_axes.c2p(1, 6, 0)))
+            MathTex("(x,", "2x + 4", ")").scale(0.7).next_to(coordinate_axes.c2p(0.9, 6, 0)))
         line_intersection_point2 = VGroup(
             coordinate_axes.get_vertical_line(coordinate_axes.c2p(1, -5, 0), color = BLUE),
             Dot(point = coordinate_axes.c2p(1, -2.5, 0), color = YELLOW),
@@ -77,8 +77,10 @@ class Problem12754(MovingCameraScene):
         self.play(Write(VGroup(line_intersection_point2[0:2]), run_time = RUN_TIME_SPEED))
         self.play(Write(line_intersection_point2[-1]))
         self.wait()
-        self.play(self.camera.frame.animate.scale(0.8).shift(RIGHT * 0.5 + UP * 0.3))
+
+        self.play(self.camera.frame.animate.scale(0.8).shift(RIGHT * 0.5 + UP * 0.6))
         self.wait()
+
         self.play(AnimationGroup(
             VGroup(line_intersection_point1[0], line_intersection_point2[0]).animate(run_time = 2.5).shift(RIGHT * 2.38),
             line_intersection_point1[1].animate(run_time = 2.5).move_to(graphs_intersection_point),
@@ -88,8 +90,8 @@ class Problem12754(MovingCameraScene):
                 RIGHT * 1.2 + UP * 0.02),
             ))
         self.wait(2)
-        self.play(self.camera.frame.animate.restore())
-        self.wait()
+
+
         self.play(FadeOut(formula1, formula2))
         self.wait()
         surrounding_rectangles = VGroup(SurroundingRectangle(line_intersection_point1[2][1], color = YELLOW, buff=0.1),
@@ -100,10 +102,10 @@ class Problem12754(MovingCameraScene):
 
         # find intersection point
         equations = VGroup(MathTex("2x+4=4.5x-7", font_size = FONT_SIZE).shift(UP * 3 + LEFT * 4),
-                           MathTex("2x-4.5x=-7-4", font_size = FONT_SIZE).shift(UP * 2.2 + LEFT * 4.5),
-                           MathTex("-2.5x=-11", font_size = FONT_SIZE).shift(UP * 1.4 + LEFT * 4.43),
-                           MathTex(r"x=\frac{-11}{-2.5}=4.4", font_size = FONT_SIZE).shift(UP * 0.4 + LEFT * 5),
-                           MathTex("2x+4=2\cdot 4.4+4=12.8", font_size = FONT_SIZE).shift(DOWN * 0.6 + LEFT * 3.05)
+                           MathTex("2x-4.5x=-7-4", font_size = FONT_SIZE).shift(UP * 2.2 + LEFT * 4.52),
+                           MathTex("-2.5x=-11", font_size = FONT_SIZE).shift(UP * 1.4 + LEFT * 4.46),
+                           MathTex(r"x=\frac{-11}{-2.5}=4.4", font_size = FONT_SIZE).shift(UP * 0.4 + LEFT * 5.03),
+                           MathTex("2x+4=2\cdot 4.4+4=12.8", font_size = FONT_SIZE).shift(DOWN * 0.6 + LEFT * 2.95)
                            )
         curved_arrows = VGroup(
             CurvedArrow(equations[0][0][3].get_bottom(), equations[0][0][9].get_bottom(), tip_length=0.15,
@@ -111,8 +113,9 @@ class Problem12754(MovingCameraScene):
             CurvedArrow(equations[0][0][5].get_top(), equations[0][0][2].get_top(), tip_length=0.15,
                         angle=.6).shift(UP * 0.1)
         )
-
+        self.wait()
         self.play(AnimationGroup(
+            self.camera.frame.animate.restore(),
             TransformFromCopy(line_intersection_point1[2][1], equations[0][0][0:4]),
             TransformFromCopy(line_intersection_point2[2][1], equations[0][0][5:]), run_time = RUN_TIME_SPEED))
         self.play(Write(equations[0][0][4]))
@@ -121,7 +124,7 @@ class Problem12754(MovingCameraScene):
         self.wait()
 
         self.play(VGroup(coordinate_axes, graph1, graph2, graphs_intersection_point, point_x1,
-                         line_intersection_point1, line_intersection_point2).animate.shift(RIGHT * 2))
+                         line_intersection_point1, line_intersection_point2).animate.shift(RIGHT * 1.85))
         self.wait(2)
 
         self.play(Write(curved_arrows[0]))
