b51a8a94f8e6f3e1c666e4a55b8d13efc93d7812
LilitM <lilit.margaryan@naghashyan.com>
2023-05-04 19:17:59 +0400

    fixes

--- scenes/enumerated/problem_12804/problem_12804.py
+++ scenes/enumerated/problem_12804/problem_12804.py
@@ -1,12 +1,12 @@
-from manim import Scene, FadeIn, Tex, MathTex, Write, VGroup, AnimationGroup, Transform, Indicate, Table, \
-    ReplacementTransform, Group, Wiggle, ORANGE, FadeOut, CurvedArrow
-from manim import UL, UP, UR, DOWN, RIGHT, LEFT, ORIGIN
+from manim import Scene, FadeIn, Tex, Write, VGroup, AnimationGroup, \
+    ReplacementTransform, ORANGE, FadeOut, CurvedArrow, SurroundingRectangle, YELLOW
+from manim import UP, DOWN, RIGHT, LEFT
 from qarakusiscene import TaskNumberBox
 from objects import BagOfMandarins, SimpleSVGMobject
-from numpy import pi, array
 from .text import *
 
 FONT_SIZE = 47
+FORMULA_FONT_SIZE=65
 
 
 class Problem12804(Scene):
@@ -17,123 +17,167 @@ class Problem12804(Scene):
         self.wait()
 
         # Fade in the bookshelf
-        bookshelf = SimpleSVGMobject("banana").shift(DOWN)
+        bookshelf = SimpleSVGMobject("star").shift(DOWN)
 
         self.play(FadeIn(bookshelf))
         self.wait()
 
         # problem settings
-        problem_settings = VGroup(
+        self.problem_settings = problem_settings = VGroup(
             Tex(problem_setting_1_part_1, problem_setting_1_part_2, font_size=FONT_SIZE).to_edge(UP).shift(
                 DOWN * 0.5).align_to(taskNumber, LEFT),
             Tex(problem_setting_2_part_1, problem_setting_2_part_2, font_size=FONT_SIZE).to_edge(UP).shift(
                 DOWN * 1.2).align_to(taskNumber, LEFT),
-            Tex(problem_setting_part3, font_size=FONT_SIZE).to_edge(UP).shift(DOWN * 1.9).align_to(taskNumber, LEFT))
+            Tex(problem_setting_part3, font_size=FONT_SIZE).to_edge(UP).shift(DOWN * 1.9))
 
         problem_settings.set_color_by_gradient("#CFC748", "#7FC381")
 
-        for item in problem_settings:
-            self.play(Write(item, run_time=2.2))
-            self.wait(1.5)
+        self.play(Write(problem_settings[0], run_time=2.2))
+        self.wait(1.5)
 
-        book_count_variable = Tex(book_count_x, font_size=FONT_SIZE).shift(UP * 0.6),
+        self.play(Write(problem_settings[1], run_time=2))
+        self.wait(1.5)
+
+        self.play(Write(problem_settings[2], run_time=1))
+        self.wait(2)
+
+        self.book_count_variable=book_count_variable = Tex(book_count_x, font_size=FONT_SIZE).shift(UP * 0.6),
 
         self.play(ReplacementTransform(bookshelf, book_count_variable[0]))
         self.wait()
 
-        # write problem settings part 1 in terms of x
-        self.play(problem_settings[0].animate.shift(DOWN * 3.3))
-        self.wait()
+        self.equality_sign=equality_sign=Tex(" $=$ ", font_size=FORMULA_FONT_SIZE).move_to(problem_settings[-1]),
 
-        helpers_for_part1 = VGroup(
-            Tex("$(x - 4)$", font_size=FONT_SIZE).next_to(problem_settings[0][1], LEFT),
-            Tex("$\cdot 5 $", font_size=FONT_SIZE).next_to(problem_settings[0][0], RIGHT).shift(LEFT * 0.2)
-        )
-        helpers_for_part1[0][0][0].set_opacity(0)
-        helpers_for_part1[0][0][-1].set_opacity(0)
-
-        self.play(ReplacementTransform(problem_settings[0][0], helpers_for_part1[0]))
-        self.wait()
-
-        self.play(ReplacementTransform(problem_settings[0][1], helpers_for_part1[1]),
-                  AnimationGroup(
-                      helpers_for_part1[0][0][0].animate.set_opacity(1),
-                      helpers_for_part1[0][0][-1].animate.set_opacity(1)
-                  ))
-        self.wait()
-
-        self.play(helpers_for_part1.animate.move_to(book_count_variable[0]).shift( LEFT * 4.5))
-        self.wait(2)
-
-        # write problem settings part 2 in terms of x
-        self.play(problem_settings[1].animate.shift(DOWN * 2.6))
-        self.wait()
-
-        helpers_for_part2 = VGroup(
-            Tex("$(x - 8)$", font_size=FONT_SIZE).next_to(problem_settings[1][1], LEFT).shift(RIGHT * 0.1),
-            Tex("$\cdot 7 $", font_size=FONT_SIZE).next_to(problem_settings[1][0], RIGHT).shift(LEFT * 0.2)
-        )
-        helpers_for_part2[0][0][0].set_opacity(0)
-        helpers_for_part2[0][0][-1].set_opacity(0)
-
-        self.play(ReplacementTransform(problem_settings[1][0], helpers_for_part2[0]))
-        self.wait()
-
-        self.play(ReplacementTransform(problem_settings[1][1], helpers_for_part2[1]),
-                  AnimationGroup(
-                      helpers_for_part2[0][0][0].animate.set_opacity(1),
-                      helpers_for_part2[0][0][-1].animate.set_opacity(1)
-                  ))
-        self.wait()
-
-        self.play(AnimationGroup(helpers_for_part2.animate.next_to(helpers_for_part1, RIGHT).shift(RIGHT * 0.55),
-                                 FadeOut(book_count_variable[0])))
-        self.wait()
+        self.convert_problem_setting_1()
+        self.convert_problem_setting_2()
 
         # combine problem settings
 
-        helpers_for_part3 = VGroup(
-            Tex("$=$", font_size=FONT_SIZE).next_to(helpers_for_part1, RIGHT),
-            Tex("$x\cdot 5- 4\cdot 5 = x\cdot 7 - 8\cdot 7$", font_size=FONT_SIZE).move_to(helpers_for_part1).shift(
-                DOWN * 0.7 + RIGHT*1.35),
-            Tex("$5x - 20 = 7x - 56$", font_size=FONT_SIZE).move_to(helpers_for_part1).shift(DOWN * 1.4 +RIGHT*1.35),
-            Tex("$5x - 7x = - 56 + 20$", font_size=FONT_SIZE).move_to(helpers_for_part1).shift(DOWN * 2.1 + RIGHT*1.5),
-            Tex("$-2x = -36$", font_size=FONT_SIZE).move_to(helpers_for_part1).shift(DOWN * 2.8 + RIGHT*1.33),
-            Tex(r"$x=\frac{-36}{-2} = 18$", font_size=FONT_SIZE).move_to(helpers_for_part1).shift(
-                DOWN * 3.5 + RIGHT*2.18)
+        converted_setting_part1 = self.converted_setting_part1
+
+        helpers = VGroup(
+            Tex("$x\cdot 5- 4\cdot 5 = x\cdot 7 - 8\cdot 7$", font_size=FORMULA_FONT_SIZE).move_to(
+                converted_setting_part1).shift(
+                UP * 0.2 + RIGHT * 1.75),
+            Tex("$5x - 20 = 7x - 56$", font_size=FORMULA_FONT_SIZE).move_to(converted_setting_part1).shift(
+                DOWN * .6 + RIGHT * 1.75),
+            Tex("$5x - 7x = - 56 + 20$", font_size=FORMULA_FONT_SIZE).move_to(converted_setting_part1).shift(
+                DOWN * 1.4 + RIGHT * 2),
+            Tex("$-2x = -36$", font_size=FORMULA_FONT_SIZE).move_to(converted_setting_part1).shift(DOWN * 2.2 + RIGHT * 1.75),
+            Tex(r"$x=\frac{-36}{-2} = 18$", font_size=FORMULA_FONT_SIZE).move_to(converted_setting_part1).shift(
+                DOWN * 3 + RIGHT * 2.9)
 
         )
 
-        self.play(ReplacementTransform(problem_settings[2], helpers_for_part3[0]))
+        self.play(AnimationGroup(ReplacementTransform(problem_settings[2], equality_sign[0]),
+                                 VGroup(self.converted_setting_part1,self.converted_setting_part2).animate.shift(UP)))
         self.wait()
 
         self.play(AnimationGroup(
-            helpers_for_part1[1][0][-1].animate.set_color(ORANGE),
-            helpers_for_part2[1][0][-1].animate.set_color(ORANGE),
+            converted_setting_part1[1][0][-1].animate.set_color(ORANGE),
+            self.converted_setting_part2[1][0][-1].animate.set_color(ORANGE),
         ))
         self.wait()
 
-        for i in range(1,3):
-            self.play(Write(helpers_for_part3[i],run_time=1.5))
+        for i in range(0, 2):
+            self.play(Write(helpers[i], run_time=1.5))
             self.wait()
         self.wait()
 
-        curved_arrows=VGroup(
-            CurvedArrow(helpers_for_part3[2][0][3].get_bottom(),helpers_for_part3[2][0][8].get_bottom(),tip_length=0.15,
-                                    angle=.6).shift(DOWN*0.1),
-            CurvedArrow(helpers_for_part3[2][0][6].get_top(), helpers_for_part3[2][0][2].get_top(),tip_length=0.15,
-                                    angle=.6).shift(UP*0.1)
+        curved_arrows = VGroup(
+            CurvedArrow(helpers[1][0][3].get_bottom(), helpers[1][0][9].get_bottom(), tip_length=0.15,
+                        angle=.6).shift(DOWN * 0.07),
+            CurvedArrow(helpers[1][0][6].get_top(), helpers[1][0][1].get_top(), tip_length=0.15,
+                        angle=.6).shift(UP * 0.1)
         )
 
         self.play(Write(curved_arrows[0]))
         self.play(Write(curved_arrows[1]))
         self.wait(2)
 
-        for i in range(3,6):
-            self.play(Write(helpers_for_part3[i],run_time=1.5))
+        self.play(Write(helpers[2], run_time=1.5))
+        self.wait(2)
+
+        self.play(FadeOut(curved_arrows))
+        self.wait(2)
+
+        for i in range(3, 5):
+            self.play(Write(helpers[i], run_time=1.5))
             self.wait()
 
-        solution=Tex(solution_str,font_size=FONT_SIZE).shift(UP*2)
+        solution = Tex(solution_str, font_size=FORMULA_FONT_SIZE).shift(UP * 2.5)
 
         self.play(Write(solution))
         self.wait(2)
+
+    def convert_problem_setting_1(self):
+        problem_settings = self.problem_settings[0]
+
+        self.play(problem_settings.animate.shift(DOWN * 3.3))
+        self.wait(1.5)
+
+        self.converted_setting_part1 = converted_setting_part1 = VGroup(
+            Tex("$(x - 4)$", font_size=FORMULA_FONT_SIZE).next_to(problem_settings[1], LEFT),
+            Tex("$\cdot 5 $", font_size=FORMULA_FONT_SIZE).next_to(problem_settings[0], RIGHT).shift(LEFT * 0.2)
+        )
+
+        surrounding_rectangles = VGroup(SurroundingRectangle(problem_settings[0], color=YELLOW, buff=0.15),
+                                        SurroundingRectangle(problem_settings[1], color=YELLOW, buff=0.15))
+
+        self.play(Write(surrounding_rectangles[0]))
+        self.wait(2)
+        self.play(AnimationGroup(FadeOut(surrounding_rectangles[0]),
+            ReplacementTransform(problem_settings[0], converted_setting_part1[0][0][1:-1]),
+            ))
+        self.wait()
+
+        self.play(Write(surrounding_rectangles[1]))
+        self.wait(2)
+
+        self.play(ReplacementTransform(problem_settings[1], converted_setting_part1[1]),
+                  FadeOut(surrounding_rectangles[1]),
+                  FadeIn(converted_setting_part1[0][0][0], converted_setting_part1[0][0][-1])
+                  )
+        self.wait()
+
+        self.play(AnimationGroup(self.book_count_variable[0].animate.shift(UP*2.5).scale(1.4)),
+                  converted_setting_part1.animate.next_to(self.equality_sign[0],LEFT).shift(DOWN))
+        self.wait(2)
+
+    def convert_problem_setting_2(self):
+
+        problem_settings = self.problem_settings[1]
+
+        self.play(problem_settings.animate.shift(DOWN * 2.6))
+        self.wait(1.5)
+
+        self.converted_setting_part2 = converted_setting_part2 = VGroup(
+            Tex("$(x - 8)$", font_size=FORMULA_FONT_SIZE).next_to(problem_settings[1], LEFT).shift(RIGHT * 0.1),
+            Tex("$\cdot 7 $", font_size=FORMULA_FONT_SIZE).next_to(problem_settings[0], RIGHT).shift(LEFT * 0.13)
+        )
+
+        surrounding_rectangles = VGroup(SurroundingRectangle(problem_settings[0], color=YELLOW, buff=0.15),
+                                        SurroundingRectangle(problem_settings[1], color=YELLOW, buff=0.15))
+
+        self.play(Write(surrounding_rectangles[0]))
+        self.wait(2)
+        self.play(AnimationGroup(
+            ReplacementTransform(problem_settings[0], converted_setting_part2[0][0][1:-1]),
+            FadeOut(surrounding_rectangles[0])))
+        self.wait()
+
+        self.play(Write(surrounding_rectangles[1]))
+        self.wait(2)
+
+        self.play(ReplacementTransform(problem_settings[1], converted_setting_part2[1]),
+                  FadeOut(surrounding_rectangles[1]),
+                  AnimationGroup(
+                      FadeIn(converted_setting_part2[0][0][0],converted_setting_part2[0][0][-1])
+                  ))
+        self.wait()
+
+
+        self.play(AnimationGroup(
+            converted_setting_part2.animate.next_to(self.equality_sign[0],RIGHT).shift(DOWN),
+            FadeOut(self.book_count_variable[0])))
+        self.wait()
