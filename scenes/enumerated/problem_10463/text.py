02d4391e66ebbbaa40b053b4df52c8fb604bcb32
Gor <74136975+Gor397@users.noreply.github.com>
2023-06-12 19:45:36 +0400

    Update text.py

--- scenes/enumerated/problem_10463/text.py
+++ scenes/enumerated/problem_10463/text.py
@@ -4,3 +4,10 @@ if LanguageConfig.is_armenian():
     from .armenian_text import *
 else:
     from .english_text import *
+
+__all__ = [
+    "taskNumberString",
+    "solve_equations",
+    "non_negative_str"
+]
+    
