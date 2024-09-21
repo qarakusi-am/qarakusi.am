56dc003e354639d1fec8ddaec283e33d890e2cea
Gor <simonyan.gor.397@gmail.com>
2024-09-21 10:55:49 +0400

    added minecraft function

--- lib/utilities.py
+++ lib/utilities.py
@@ -1,8 +1,8 @@
 import numpy as np
 from manim import PI, TAU
-from manim import ORANGE, BLACK
-from manim import ORIGIN
-from manim import VGroup, MathTex, AnnularSector, Point
+from manim import ORANGE, BLACK, BLUE
+from manim import ORIGIN, X_AXIS, Y_AXIS, Z_AXIS
+from manim import VGroup, MathTex, AnnularSector, Point, Cube
 
 
 def length(array):
@@ -69,3 +69,41 @@ def getFractionsOnCircle(fraction, circle, color=ORANGE, number_of_sectors=1, st
         return VGroup(sectors, numbers)
     else:
         return sectors
+
+def minecraft(matrix, side_length=2, fill_opacity=1, stroke_width=1, stroke_color=BLUE, stroke_opacity=1, **kwargs) -> VGroup:
+    """
+    Creates 3D objects using Cubes (Warning! Adjust light_source and shading_factor of ThreeDCamera to cover the flaws of 3D graphics of Manim)
+    
+    (Demo is in 'scenes/demo/minecraft/minecraft.py')
+
+    arr = [
+        [
+            [0, 1],
+            [1, 1]
+        ],
+        [
+            [0, 1],
+            [1, 1]
+        ]
+    ]
+
+    obj = minecraft(arr, fill_color=BLUE).rotate(-PI/10, X_AXIS).rotate(-PI/10, Y_AXIS)
+
+    """
+    obj = VGroup()
+    for z, layer in enumerate(matrix):
+        for y, raw in enumerate(layer):
+            for x, val in enumerate(raw):
+                if val:
+                    cube = Cube(side_length=side_length,
+                                fill_opacity=fill_opacity,
+                                stroke_width=stroke_width,
+                                stroke_color=stroke_color,
+                                stroke_opacity=stroke_opacity,
+                                **kwargs)
+                    cube.shift(x*(side_length)*X_AXIS, -y*(side_length)*Y_AXIS, -z*(side_length)*Z_AXIS)
+                    obj.add(cube)
+                else:
+                    pass
+
+    return obj.move_to(ORIGIN)
